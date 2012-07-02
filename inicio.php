2ec527620b2e8266a7c89891fd9419113c66795f
Maly-Y-Yohan <Maly-Y-Yohan@Maly-Y-Yohan-PC>
2012-07-02 08:30:55 -0430

    creeinicio

---
+++ inicio.php
@@ -0,0 +1,9 @@
+<html>
+    <body>
+        <table align="center" width="1200" height="48">
+            <tr>
+                <td> <img src="imagenes/otra.png" width="1250" height="180" /></td>
+            </tr>
+        </table>
+    </body>
+</html>
