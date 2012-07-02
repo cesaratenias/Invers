0f520d2c2dabb163fbf9c25139a5d1c1fa16c80a
Maly-Y-Yohan <Maly-Y-Yohan@Maly-Y-Yohan-PC>
2012-07-02 08:32:47 -0430

    creeinicio

--- inicio.php
+++ inicio.php
@@ -2,7 +2,7 @@
     <body>
         <table align="center" width="1200" height="48">
             <tr>
-                <td> <img src="imagenes/otra.png" width="1250" height="180" /></td>
+                <td> <img src="imagenes/otra.jpg" width="150" height="180" /></td>
             </tr>
         </table>
     </body>
