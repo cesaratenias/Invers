fad0de773de3979c5c4a7d2f6adc661f81e54769
Maly-Y-Yohan <Maly-Y-Yohan@Maly-Y-Yohan-PC>
2012-07-02 16:03:18 -0430

    cambie

--- modulos/cliente/vistas/nuevo.php
+++ modulos/cliente/vistas/nuevo.php
@@ -1,30 +1,58 @@
-<html>
-    <head>       
-    </head>
-    <body>
+<?php include '../modelos/consulta.php'; ?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+    <head>
+        <title>Registro de Cliente</title>
+        <meta http-equiv="content-type" content="text/html;charset=utf-8" />
+        <meta name="generator" content="Geany 0.19.1" />
+    </head> 
+
+    <body >
         <div align="center">
-            <form action="" method="post">
-                <table width="425" height="200">
-                    <tr>
-                        <td>Codigo</td>
-                        <td><input required type="text"/></td>
-                    </tr>
-                    <tr>
-                        <td>Descripcion</td>
-                        <td><input required type="text"/></td>
-                    </tr>
-                    <tr>
-                        <td>Costo</td>
-                        <td><input required type="text"/></td>
-                    </tr>
-                    <tr>
-                        <td>Condicion</td>
-                        <td><input required type="text"/></td>
-                    </tr>
-                </table>
-                <input type="submit" value="Guardar" />
-                <input type="reset" value="Limpiar" />
-            </form>
+            <img src="../../../imagenes/cliente.jpg" width="80" height="80" />
+            <?php
+            if ($_GET['acc'] == 'edicion') {
+                echo "Editando Cliente";
+            } else {
+                echo 'Registro de Cliente';
+            }
+            ?>
         </div>
+        <br />
+        <br />
+        <form action='../modelos/guardar.php' method='POST'>
+            <table align="center">
+                <tr>
+                    <td>C&eacute;dula:</td>
+                    <td><input type="text" name="cedula"   /></td> 
+                </tr>
+                <tr>
+                    <td align="center">&nbsp;</td>
+                    <td>Nombre:</td>
+                    <td> <input type="text" name="nombre" /></td></tr>
+                <tr>
+                    <td align="center">&nbsp;</td>
+                    <td>Apellido:</td>
+                    <td> <input type="text" name="apellido" /></td>
+                </tr>
+                <tr>
+                    <td align="center">&nbsp;</td>
+                    <td>Telefonocli:</td>
+                    <td> <input type="text" name="telefono" /></td></tr>
+                <tr>
+                    <td align="center">&nbsp;</td>
+                    <td>Direccion:</td>
+                    <td> <input type="text" name="direccion"  /></td></tr>
+            </table>
+            </font>
+
+            <table align ='center'>
+                <tr><td align ='center'><input type='submit' name="boton" value='Guardar'></td>
+                    <td><input type='submit' name="boton" value ='Cancelar'></td></tr>
+            </table>
+        </form>
+
+
     </body>
 </html>
+
