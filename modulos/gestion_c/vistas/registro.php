bf7504e2ae4091b53cb270ff932a30a8051fe2f8
Cesar Tenias <cesaratenias7@gmail.com>
2012-07-02 08:55:22 -0430

    registro gestion compra y query en suma

--- modulos/gestion_c/vistas/registro.php
+++ modulos/gestion_c/vistas/registro.php
@@ -2,7 +2,7 @@
 
 <h2 align="center">Registro de compra</h2>
 <form>
-    <table align="center" class="formulario">
+    <table align="center" class="table">
         <tr>
             <td>Fecha</td>
             <td><?php echo date('d-m-Y'); ?></td>
@@ -25,6 +25,69 @@
                 <input required type="text" name="total"/>
         </tr>
     </table>
+    <fieldset>
+        <legend>Material</legend>
+        <table>
+            <thead>
+                <tr>
+                    <th>Material</th>            
+                    <th>Cantidad</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <input/>
+                    </td>
+                    <td><input type="text" id="cantidadMP" name="cantidadMP" /></td>
+                    <td><input type="button" value="Agregar"/></td>
+                </tr>
+            </tbody>
+        </table>
+    </fieldset>
+    <fieldset>
+        <legend>Equipo</legend>
+        <table>
+            <thead>
+                <tr>
+                    <th>Equipo</th>            
+                    <th>Cantidad</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <input/>
+                    </td>
+                    <td><input type="text" id="cantidadMP" name="cantidadMP" /></td>
+                    <td><input type="button" value="Agregar"/></td>
+                </tr>
+            </tbody>
+        </table>
+    </fieldset>
+    <fieldset>
+        <legend>Mano de obra</legend>
+        <table>
+            <thead>
+                <tr>
+                    <th>Material</th>            
+                    <th>Cantidad</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <input/>
+                    </td>
+                    <td><input type="text" id="cantidadMP" name="cantidadMP" /></td>
+                    <td><input type="button" value="Agregar"/></td>
+                </tr>
+            </tbody>
+        </table>
+    </fieldset>
     <br/>
     <center><input type="submit" value="Guardar"/>&nbsp;&nbsp;<input type="button"value="Regresar" onclick="location.href='lista.php'" /></center>
     <br />
