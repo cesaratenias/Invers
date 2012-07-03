73a2776f4bac0649f677982f36810bad09f584cd
Maly-Y-Yohan <Maly-Y-Yohan@Maly-Y-Yohan-PC>
2012-07-02 21:41:23 -0430

    agregle

--- modulos/proveedor/modelos/consulta.php
+++ modulos/proveedor/modelos/consulta.php
@@ -1,6 +1,6 @@
 <?php
 include '../../../conexion.php'; 
 $id = $_GET['id'];
-$sqlCc="SELECT p.*, pro.* FROM persona p INNER JOIN proveedor pro ON p.id=pro.id_persona WHERE id.p = $id;";
+$sqlCc="SELECT p.*, pro.* FROM persona p INNER JOIN proveedor pro ON p.id=pro.id_persona WHERE p.id = $id;";
 $sql= mysql_query($sqlCc);
 ?>
