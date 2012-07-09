e3f83462884b791915f60d820f8ad7ceb912ba8e
Maly-Y-Yohan <Maly-Y-Yohan@Maly-Y-Yohan-PC>
2012-07-09 19:20:29 -0430

    agregado la clasee para para bn y no

--- modulos/cliente/modelos/editar.php
+++ modulos/cliente/modelos/editar.php
@@ -6,14 +6,12 @@ $cedula = $_POST['cedula'];
 $nombre = $_POST['nombre'];
 $telefono = $_POST['telefono'];
 $direccion = $_POST['direccion'];
-$tipo = 3;
 
 $updPro = "UPDATE persona SET ";
 $updPro.= "cedula = '$cedula', ";
 $updPro.= "nombre = '$nombre', ";
 $updPro.= "telefono = '$telefono', ";
-$updPro.= "direccion = '$direccion', ";
-$updPro.= "tipo = '$tipo' ";
+$updPro.= "direccion = '$direccion' ";
 $updPro.= "WHERE id = $id";
 
 $sql = mysql_query($updPro);
@@ -22,14 +20,14 @@ $idPer = mysql_insert_id();
 if ($sql) {
   ?>
   <center>
-      <div>Guardada satisfactoriamente</div>
+      <div class="bien">Guardada satisfactoriamente</div>
   </center>
   <script type="text/javascript" language="javascript">
-      setTimeout("location.href='../vistas/lista.php'", 3000);
+      setTimeout("location.href='../vistas/listar.php'", 3000);
   </script>
 <?php } else { ?>
   <center>
-      <div>Ha ocurrido un error, contacte al administrador</div>
+      <div class="no">Ha ocurrido un error, contacte al administrador</div>
   </center>
   <script type="text/javascript" language="javascript">
       setTimeout("location.href='../vistas/lista.php'", 3000);
